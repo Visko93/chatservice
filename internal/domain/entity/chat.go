571bb4c6b9ce2f1f15482b8c219fef1690f4d664
Vinicius Skonicezny <vskonicezny@gmail.com>
2023-04-18 00:22:09 -0300

    feat: infra openAI

--- internal/domain/entity/chat.go
+++ internal/domain/entity/chat.go
@@ -1,1 +1,107 @@
 package entity
+
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+type ChatConfig struct {
+	Model            *Model
+	Temperature      float32
+	TopP             float32
+	N                int // number of mesaages to generate
+	Stop             []string
+	MaxTokens        int // max number of tokens to generate
+	PresencePenalty  float32
+	FrequencyPenalty float32
+}
+
+type Chat struct {
+	ID                   string
+	UserID               string
+	InitialSystemMessage *Message
+	Messages             []*Message
+	ErasedMessages       []*Message
+	Status               string
+	TokenUsage           int
+	Config               *ChatConfig
+}
+
+func NewChat(userID string, InitialSystemMessage *Message, config *ChatConfig) (*Chat, error) {
+	chat := &Chat{
+		ID:                   uuid.New().String(),
+		UserID:               userID,
+		InitialSystemMessage: InitialSystemMessage,
+		Status:               "active",
+		Config:               config,
+		TokenUsage:           0,
+	}
+	chat.AddMessage(InitialSystemMessage)
+
+	if err := chat.Validate(); err != nil {
+		return nil, err
+	}
+	return chat, nil
+}
+
+func (c *Chat) AddMessage(m *Message) error {
+	if c.Status == "ended" {
+		return errors.New("chat is ended")
+	}
+	for {
+		if c.Config.Model.getMaxTokens() >= m.getQuantityTokens()+c.TokenUsage {
+			c.Messages = append(c.Messages, m)
+			c.RefreshTokenUsage()
+			break
+		}
+		c.ErasedMessages = append(c.ErasedMessages, c.Messages[0])
+		c.Messages = c.Messages[1:]
+		c.RefreshTokenUsage()
+	}
+	return nil
+}
+
+func (c *Chat) getMessages() []*Message {
+	return c.Messages
+}
+
+func (c *Chat) countMessages() int {
+	return len(c.Messages)
+}
+
+func (c *Chat) endChat() {
+	c.Status = "ended"
+}
+
+func (c *Chat) isEnded() bool {
+	return c.Status == "ended"
+}
+
+func (c *Chat) isStarted() bool {
+	return c.Status == "active"
+}
+
+func (c *Chat) startChat() {
+	c.Status = "active"
+}
+
+func (c *Chat) Validate() error {
+	if c.UserID == "" {
+		return errors.New("user id is empty")
+	}
+	if c.Status != "started" && c.Status != "ended" {
+		return errors.New("invalid status")
+	}
+	if c.Config.Temperature < 0.0 || c.Config.Temperature > 5.0 {
+		return errors.New("invalid temperature")
+	}
+	return nil
+}
+
+func (c *Chat) RefreshTokenUsage() {
+	c.TokenUsage = 0
+	for m := range c.Messages {
+		c.TokenUsage += c.Messages[m].getQuantityTokens()
+	}
+}
--- internal/domain/entity/message.go
+++ internal/domain/entity/message.go
@@ -1,1 +1,51 @@
 package entity
+
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+	tiktoken_go "github.com/j178/tiktoken-go"
+)
+
+type Message struct {
+	ID        string
+	Role      string
+	Content   string
+	Tokens    int
+	Model     *Model
+	CreatedAt time.Time
+}
+
+func (m *Message) Validate() error {
+	if m.Role != "user" && m.Role != "system" && m.Role != "assistant" {
+		return errors.New("invalid role")
+	}
+	if m.Content == "" {
+		return errors.New("content is empty")
+	}
+	if m.CreatedAt.IsZero() {
+		return errors.New("invalid created_at")
+	}
+	return nil
+}
+
+func (m *Message) getQuantityTokens() int {
+	return m.Tokens
+}
+
+func NewMessage(role, content string, model *Model) (*Message, error) {
+	totalTokens := tiktoken_go.CountTokens(model.getModelName(), content)
+	msg := &Message{
+		ID:        uuid.New().String(),
+		Role:      role,
+		Content:   content,
+		Tokens:    totalTokens,
+		Model:     model,
+		CreatedAt: time.Now(),
+	}
+	if err := msg.Validate(); err != nil {
+		return nil, err
+	}
+	return msg, nil
+}
